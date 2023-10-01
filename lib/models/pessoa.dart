0db0fe915debbba389a57186a19e51df2da2f6d6
josemailson <jmailsons@gmail.com>
2023-10-01 10:34:41 -0300

    add: classificacao in class pessoa

--- lib/models/pessoa.dart
+++ lib/models/pessoa.dart
@@ -1,3 +1,5 @@
+import 'package:calculadora_imc/enum/classificacao.dart';
+
 class Pessoa {
   String _nome = '';
   double _peso = 0;
@@ -33,13 +35,35 @@ class Pessoa {
     return _peso / (_altura * _altura);
   }
 
+  Classificacao classificarIMC() {
+    var imc = calcularIMC();
+    if (imc < 16) {
+      return Classificacao.magrezaGrave;
+    } else if (imc < 17) {
+      return Classificacao.magrezaModerada;
+    } else if (imc < 18.5) {
+      return Classificacao.magrezaLeve;
+    } else if (imc < 25) {
+      return Classificacao.saudavel;
+    } else if (imc < 30) {
+      return Classificacao.sobrepeso;
+    } else if (imc < 35) {
+      return Classificacao.obesidadeGrauI;
+    } else if (imc < 40) {
+      return Classificacao.obesidadeGrauII;
+    } else {
+      return Classificacao.obesidadeGrauIII;
+    }
+  }
+
   @override
   String toString() {
     return {
       'Nome': _nome,
       'Peso': _peso,
       'Altura': _altura,
-      'IMC': calcularIMC()
+      'IMC': calcularIMC().toStringAsFixed(2),
+      'Classificação': enumToString(classificarIMC())
     }.toString();
   }
 }
